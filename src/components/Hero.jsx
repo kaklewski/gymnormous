e4f9c024f5d3c63a2c5c49b751fe0b8ef6d74fad
Oskar <oskarkaklewski@gmail.com>
2024-07-09 19:45:22 +0200

    Added styling to the Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
 export default function Hero() {
-	return <div>
-    <p>hero</p>
-  </div>;
+	return (
+		<div className='min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto p-4'>
+			<div className='flex flex-col  gap-4'>
+				<p>IT'S TIME TO GET</p>
+				<h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>
+					RIPPED
+				</h1>
+			</div>
+			<p className='text-sm md:text-base font-light'>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+				Similique maiores{' '}
+				<span className='text-blue-400 font-medium'>magnam</span> quam
+				numquam maxime laudantium, laboriosam distinctio provident iusto
+				ipsum.
+			</p>
+			<button className='px-8 py-4 rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-200'>
+				<p>Accept & Begin</p>
+			</button>
+		</div>
+	);
 }
