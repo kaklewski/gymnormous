b68adaff1fe0fa899a940a0e4c5e98b0c3b80e25
Oskar <oskarkaklewski@gmail.com>
2024-07-09 20:10:29 +0200

    Added the Header component

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,5 +1,29 @@
 import React from 'react';
+import SectionWrapper from './SectionWrapper';
+
+function Header(props) {
+	const { index, title, description } = props;
+	return (
+		<div>
+			<div>
+				<p>{index}</p>
+				<h4>{title}</h4>
+			</div>
+			<p>{description}</p>
+		</div>
+	);
+}
 
 export default function Generator() {
-	return <div>gene</div>;
+	return (
+		<SectionWrapper
+			header={'generate your workout'}
+			title={['Its', 'Huge', "o'clock"]}>
+			<Header
+				index={'01'}
+				title={'Pick your poison'}
+				description={'Select the workout you wish to endure.'}
+			/>
+		</SectionWrapper>
+	);
 }
