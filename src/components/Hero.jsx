9aa7bef5d7442b3c8b58fc95cb5be93b9aa9a121
Oskar Kąklewski <105233545+kaklewski@users.noreply.github.com>
2024-09-25 17:43:55 +0200

    Changed the app name

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Button from './Button';
+import React from 'react'
+import Button from './Button'
 
 export default function Hero() {
 	return (
@@ -7,14 +7,14 @@ export default function Hero() {
 			<div className='flex flex-col gap-4'>
 				<p>IT'S TIME TO GET</p>
 				<h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>
-					Swole<span className='text-blue-400'>normous</span>
+					GYM<span className='text-blue-400'>normous</span>
 				</h1>
 			</div>
 
 			<p className='text-sm md:text-base font-light'>
 				I hereby acknowledgement that I may become{' '}
 				<span className='text-blue-400 font-medium'>
-					unbelievably swolenormous
+					unbelievably GYMnormous
 				</span>{' '}
 				and accept all risks of becoming the local{' '}
 				<span className='text-blue-400 font-medium'>
@@ -24,9 +24,12 @@ export default function Hero() {
 				doors.
 			</p>
 
-			<Button func={() => {
-				window.location.href = '#generate'
-			}} text={'Accept & Begin'} />
+			<Button
+				func={() => {
+					window.location.href = '#generate'
+				}}
+				text={'Accept & Begin'}
+			/>
 		</div>
-	);
+	)
 }
